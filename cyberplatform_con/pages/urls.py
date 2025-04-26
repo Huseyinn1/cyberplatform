41d2f4fef3f08b69f13fde49be10ec0621a7d164
Hüseyin Adıgüzel <ahuseyin235@gmail.com>
2025-04-26 15:36:09 +0300

    Ocr uygulaması eklendi

--- cyberplatform_con/pages/urls.py
+++ cyberplatform_con/pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from pages.views import AboutView,IndexView
+from pages.views import AboutView,IndexView,analyze_image
 
 urlpatterns = [
    path('',IndexView.as_view(),name="index"),
     path('about/',AboutView.as_view(),name="about"),
+      path('ocr/', views.ocr_view, name='ocr'),
    
     
 ]
--- cyberplatform_con/pages/views.py
+++ cyberplatform_con/pages/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from blogs.models import Blog
 
+
+# views.py
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
+
 # Create your views here.
 #def index(request):
  #   return render(request,'index.html')
@@ -17,4 +23,14 @@ class AboutView(TemplateView):
     template_name = 'about.html' 
 #def about(request):
   #  return render(request,'about.html')
+def ocr_view(request):
+    return render(request, 'ocr.html')
 
+@csrf_exempt  # CSRF korumasını geçici olarak devre dışı bırakıyoruz (gerçek uygulamalarda CSRF'yi uygun şekilde yapılandırmalısınız)
+def analyze_image(request):
+    if request.method == 'POST' and request.FILES.get('image'):
+        image = request.FILES['image']
+        # Burada, dosya üzerinde işlemler yapabilirsiniz
+        file_name = default_storage.save(image.name, image)
+        return JsonResponse({'message': 'Dosya başarıyla alındı', 'file_name': file_name}, status=200)
+    return JsonResponse({'error': 'Geçersiz istek'}, status=400)
